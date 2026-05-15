cc327f55e55623ffa9140496898f4a225501adce
lee0806 <leeseh0806@gmail.com>
2026-05-15 23:54:45 +0900

    [COD-29] feat: Repository 구성, Oauth, User
    Signed-off-by: lee0806 <leeseh0806@gmail.com>

---
+++ src/main/java/com/ide/project/domain/user/reposigory/OauthAccountRepository.java
@@ -0,0 +1,18 @@
+package com.ide.project.domain.user.reposigory;
+
+import com.ide.project.domain.user.entity.OauthAccount;
+import com.ide.project.domain.user.entity.User;
+import com.ide.project.domain.user.entity.Provider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+// 인터페이스로 구현한 이유는 Spring이 실행 시점에서 자동으로 구현체를 만들어주기 때문
+public interface OauthAccountRepository extends JpaRepository<OauthAccount, Long> {
+
+    // 소셜 로그인시 이미 연동된 계정인지 확인, 카카오로 로그인한 유저가 고유 ID로 기존 계정을 조회할 때
+    Optional<OauthAccount> findByProviderAndProviderId(Provider provider, String providerId);
+
+    // 특정 유저가 특정 소셜 로그인을 이미 연동했는지 확인할 때
+    Optional<OauthAccount>findByUserAndProvider(User user, Provider provider);
+}
---
+++ src/main/java/com/ide/project/domain/user/reposigory/UserRepository.java
@@ -0,0 +1,15 @@
+package com.ide.project.domain.user.reposigory;
+
+import com.ide.project.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 이메일로 유저 찾기
+    Optional<User> findByEmail(String email);
+
+    // 중복된 이메일이 있는지 확인
+    boolean existsByEmail(String email);
+}
