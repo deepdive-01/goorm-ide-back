929aff0d0379fd65b39dfb2e06c61a0e307faba7
lee0806 <leeseh0806@gmail.com>
2026-05-18 12:41:29 +0900

    [COD-29] 인증 관련 엔드포인트 아무나 접근 허용 및 JWT 필터 등록
    Signed-off-by: lee0806 <leeseh0806@gmail.com>

--- src/main/java/com/ide/project/global/config/SecurityConfig.java
+++ src/main/java/com/ide/project/global/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ide.project.global.config;
 
+import com.ide.project.global.security.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -17,18 +20,34 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor // Filter 주입
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        // 인증 없이 접근 가능한 엔드포인트
+                        .requestMatchers(
+                                "/api/v1/auth/email/send",
+                                "/api/v1/auath/email/verify",
+                                "/api/v1/auth/signup",
+                                "/api/v1/auth/refresh"
+                        ).permitAll()
+                        // Swagger UI
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        // 나머지 요청은 인증을 요구
+                        .anyRequest().authenticated()
                 )
+                // JWT 필터를 등록
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
