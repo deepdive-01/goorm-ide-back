f0685e8acdff8dee43b927e5fd7bbebc32a29477
jgu330 <gyeoun330@gmail.com>
2026-05-20 14:47:28 +0900

    [COD-35] space entity제거 및 수정

--- src/main/java/com/ide/project/domain/files/repository/SpaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ide.project.domain.files.repository;
-
-import com.ide.project.domain.files.entity.Space;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpaceRepository extends JpaRepository<Space, Long> {
-}
