236d049b36cb6d4c14610f7e7d2ffb31c4f00cad
jgu330 <gyeoun330@gmail.com>
2026-05-21 17:29:01 +0900

    entity 변수 값 변경으로 인한 service 변경

--- src/main/java/com/ide/project/domain/files/service/FileService.java
+++ src/main/java/com/ide/project/domain/files/service/FileService.java
@@ -1,11 +1,18 @@
 package com.ide.project.domain.files.service;
 
-import com.ide.project.domain.files.dto.CodeUpdateRequest;
-import com.ide.project.domain.files.dto.ProblemCreateRequest;
-import com.ide.project.domain.files.dto.ProblemResponse;
+import com.ide.project.domain.files.dto.*;
+import java.util.List;
 
 public interface FileService {
+
     ProblemResponse createProblem(ProblemCreateRequest request);
-    void updateCodeSubmission(CodeUpdateRequest request);
+    ProblemResponse updateProblem(Long problemId, ProblemUpdateRequest request);
+    ProblemResponse assignProblemFromBank(ProblemAssignRequest request);
     ProblemResponse getProblemDetails(Long problemId);
+
+
+    void saveTestCases(Long problemId, List<TestCaseCreateRequest> testCaseRequests);
+
+
+    void submitCode(SubmissionRequest request);
 }
