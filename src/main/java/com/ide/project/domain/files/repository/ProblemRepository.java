db64d310a29fdbec8615f517ecfdc8808b806f98
jgu330 <gyeoun330@gmail.com>
2026-05-21 17:26:32 +0900

    repository 보완 및 수정

--- src/main/java/com/ide/project/domain/files/repository/ProblemRepository.java
+++ src/main/java/com/ide/project/domain/files/repository/ProblemRepository.java
@@ -5,5 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
+    
+    // 특정 스페이스(워크스페이스)에 속한 모든 문제 조회
     List<Problem> findAllBySpaceId(Long spaceId);
+
+    // 특정 강사(생성자)가 등록한 모든 문제 조회 (createdBy 필드명 반영)
+    List<Problem> findAllByCreatedBy(Long createdBy);
 }
--- src/main/java/com/ide/project/domain/files/repository/SubmissionRepository.java
+++ src/main/java/com/ide/project/domain/files/repository/SubmissionRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
+    
+    // 특정 유저가 특정 문제에 대해 제출한 기록 단건 조회
     Optional<Submission> findByProblemIdAndUserId(Long problemId, Long userId);
 }
