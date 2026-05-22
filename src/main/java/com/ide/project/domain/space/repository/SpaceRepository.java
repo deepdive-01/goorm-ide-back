d7da2f66c2a966f98495ccd47840df7ba8690059
lee0806 <leeseh0806@gmail.com>
2026-05-22 11:35:04 +0900

    [COD-38] SpaceMemberRepository, SpaceRepository 구현
    Signed-off-by: lee0806 <leeseh0806@gmail.com>

--- src/main/java/com/ide/project/domain/space/repository/SpaceRepository.java
+++ src/main/java/com/ide/project/domain/space/repository/SpaceRepository.java
@@ -1,7 +1,18 @@
 package com.ide.project.domain.space.repository;
 
 import com.ide.project.domain.space.entity.Space;
+import com.ide.project.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SpaceRepository extends JpaRepository<Space, Long> {
+
+    // 멘토가 생성한 스페이스 목록
+    List<Space> findByMentor(User mentor);
+
+    // 초대코드로 스페이스 찾기
+    Optional<Space> findByInviteCodeAndIsActiveTrue(String inviteCode);
+
 }
